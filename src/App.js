bd642b663b2800649a7a4581db8edd6c1b97baec
dLucs <99974795+dLucs@users.noreply.github.com>
2022-10-14 14:50:53 +0200

    Added all files

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React from "react";
+import Die from "./Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+  const [tries, setTries] = React.useState(1);
+  const [time, setTime] = React.useState(0);
+  const [running, setRunning] = React.useState(true);
+
+  const seconds = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
+  const minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
+
+  React.useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      setRunning(false);
+    }
+  }, [dice]);
+
+  function Stopwatch() {
+    React.useEffect(() => {
+      let interval;
+      if (running) {
+        interval = setInterval(() => {
+          setTime((prevTime) => prevTime + 10);
+        }, 10);
+      } else if (!running) {
+        clearInterval(interval);
+      }
+      return () => clearInterval(interval);
+    }, [running]);
+    return (
+      <h3 className="timer">
+        {" "}
+        Time <span>{minutes}</span>:<span>{seconds}</span>
+      </h3>
+    );
+  }
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    if (!tenzies) {
+      setRunning(true);
+      setTries(tries + 1);
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+      setTries(1);
+      setTime(0);
+      setRunning(true);
+    }
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  return (
+    <main>
+      {tenzies && (
+        <Confetti
+          width={window.innerWidth || 300}
+          height={window.innerHeight || 200}
+        />
+      )}
+      <div className="top">
+        <Stopwatch />
+        <h1 className="title">Tenzies</h1>
+        <h3 className="counter"> Roll {tries}</h3>
+      </div>
+
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </main>
+  );
+}
